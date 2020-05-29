46f8dc6ff41e89a0e0185468b727a53bbdcaa42f
Otavio Barreto de Oliveira Silva <otaviosilva2632@gmail.com>
2020-05-29 15:40:32 -0300

    finished project

---
+++ app.js
@@ -0,0 +1,31 @@
+
+AOS.init();
+
+const getPokemoUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const generatePokemonPromises = () => Array(150).fill().map((_, index) => fetch(getPokemoUrl(index + 1)).then(res => res.json()))
+
+const generateHTML = pokemons =>  pokemons.reduce((accumulator, {name, id, types}) => {
+  const elementTypes = types.map(type => type.type.name)
+  accumulator += `
+    <li class="card ${elementTypes[0]}" data-aos="fade-left">
+      <img class="card-image" alt="${name}" src="https://pokeres.bastionbot.org/images/pokemon/${id}.png"/>
+      <h2 class="card-title">${id}. ${name}</h2>
+      <p class="card-subtitle">
+        ${elementTypes.join(" | ")}
+      </p>
+    </li>
+  `
+  return accumulator
+}, '')
+    
+const insertPokemonsIntoPage = pokemons => {
+  const ul = document.querySelector('[data-js="pokedex"]')
+  ul.innerHTML = pokemons
+}
+
+const pokemonPromises = generatePokemonPromises()
+Promise.all(pokemonPromises)
+  .then(generateHTML)
+  .then(insertPokemonsIntoPage)
+
